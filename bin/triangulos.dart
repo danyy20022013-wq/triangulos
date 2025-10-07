78131a359289ba70f5757a559e5826600381a64e
daniel barron <danyy20022013@gmail.com>
2025-10-07 13:40:40 -0700

    este es el metodo para calcular lo que hiciemos en el test

--- bin/triangulos.dart
+++ bin/triangulos.dart
@@ -1,5 +1,4 @@
-import 'package:triangulos/triangulos.dart' as triangulos;
 
 void main(List<String> arguments) {
-  print('Hello world: ${triangulos.calculate()}!');
+  
 }
--- lib/triangulos.dart
+++ lib/triangulos.dart
@@ -1,3 +1,15 @@
-int calculate() {
-  return 6 * 7;
-}
+
+  int ClasificarTriangulo (int a, int b, int c){
+    if (a <=0 || b<=0 || c<=0){
+      return 0 ;
+    }
+    if( (a+b <=c) || (a+c <=b) || (b+c <=a)){
+      return 4;
+    }
+    if( (a==b && b==c)){
+      return 1;
+    }
+    return 3;
+  }
+
+
